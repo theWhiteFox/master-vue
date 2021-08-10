b8efaab1bbe9acc040b1d859787b72e0bc170a02
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2021-08-10 12:35:58 +0100

    Vue Object this

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn Vue!'
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: 'Master Vue to build Magically Fab Apps!',
+            vueLink: 'https://vuejs.org'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if(randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 });
 
