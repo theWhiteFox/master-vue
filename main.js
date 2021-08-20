7d8ca39ad3e54ca7cd4356abdb7e840b6171b728
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2021-08-20 14:08:42 +0100

    the set up

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            boxASelected: false,
-            boxBSelected: false,
-            boxCSelected: false,
+            items: []
         };
     },
-    methods: {
-        boxSelected(box) {
-            if (box === 'A') {
-                this.boxASelected = !this.boxASelected;
-            } else if (box == 'B') {
-                this.boxBSelected = !this.boxBSelected;
-            } else if (box === 'C') {
-                this.boxCSelected = !this.boxCSelected;
-            }
-        }
-    }
 });
 
-app.mount('#styling');
+app.mount('#todo');
