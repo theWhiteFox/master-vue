8bde3ce1aaa4e882cb111bbad03cf6bf4e625249
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2021-08-21 12:04:15 +0100

    Game Set Up

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            enteredItemVal: '',
-            items: []
+  
         };
     },
     methods: {
-        addItem() {
-            this.items.push(this.enteredItemVal);
-        },
-        removeItem(i) {
-            this.items.splice(i, 1);
-        }
+
     },
 });
 
-app.mount('#todo');
+app.mount('#game');
